6ed577dc285407a9d33529b2a65d8017187b9320
Eric Bishop <Bishoeri000@warren.k12.in.us>
2024-01-11 09:11:26 -0500

    commit

---
+++ Unit 01_JavaScript_Basics and DOM Manipulation/03_Statements, Syntax, Comments, Variables, and Prompt/script.js
@@ -0,0 +1,6 @@
+let firstName = prompt("Enter Your Name");
+let lastName = prompt("Enter Your Name");
+
+let fullName = firstName + " " + lastName;
+
+document.getElementById("name").innerHTML = fullName;
