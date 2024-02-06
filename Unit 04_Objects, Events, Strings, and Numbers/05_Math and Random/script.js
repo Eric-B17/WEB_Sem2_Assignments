fd4223643f54c65eb54a7bee90f3cbda8905e687
Eric Bishop <Bishoeri000@warren.k12.in.us>
2024-02-06 09:11:27 -0500

    commit

---
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script.js
@@ -0,0 +1,6 @@
+function takeSquareRoot(){
+    let number = +document.getElementById("input1").value;
+
+    let squareRoot = Math.sqrt(number);
+    document.getElementById("result").innerHTML = squareRoot;
+}
