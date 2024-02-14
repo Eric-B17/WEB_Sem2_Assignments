cfbd324a295249cda7b133e6034426717515696c
Eric Bishop <Bishoeri000@warren.k12.in.us>
2024-02-14 09:12:09 -0500

    commit

--- Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script1.js
+++ Unit 04_Objects, Events, Strings, and Numbers/05_Math and Random/script1.js
@@ -1,4 +1,3 @@
-HEAD
 function takeSquareRoot(){
     let number = +document.getElementById("input1").value;
 
@@ -6,13 +5,5 @@ function takeSquareRoot(){
     document.getElementById("result").innerHTML = squareRoot;
 }
 
-function takeSquareRoot() {
-    let number = +document.getElementById("input1").value;
-    
-
-    let squareRoot = Math.sqrt(number);
-    document.getElementById("result").innerHTML = squareRoot;
-    
-}
 
 // Make another function for takeAbsoluteValue
